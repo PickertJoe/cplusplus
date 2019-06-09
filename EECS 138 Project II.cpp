aee88779c3bfe474814203f2a369bd4067bdb255
Joseph Pickert <pickertjoe@gmail.com>
2019-06-09 16:49:14 -0600

    Adding completed versions of final 2 projects

---
+++ EECS 138 Project II.cpp
@@ -0,0 +1,137 @@
+//********************************************************
+// Name:        Joe Pickert
+// KUID:        2798865
+// Section:     MWF 10:00-10:50
+// Instructor:  Scott Coston
+// Assignment:  Spring 2018 Project 2
+//********************************************************
+//
+//
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void print (double exam[], int no_of_exams, double lab[], int no_of_labs, double project[], int no_of_projects, double weighted_mean, double best_of_project, double average_of_labs, double total_marks, char grade, string comments, string name)
+{   cout<<"\nStudent name is: "<<name<<"\n";
+    cout<<"Grade is "<<grade<<"\n\n";
+    ofstream fout("Pickert_ProjectII.txt");
+    fout<<"Sample file output starts next:\n\n";
+    fout<<"Student name: "<<name<<"\n\n";
+    for(int i=0; i<no_of_projects; i++)
+        {fout<<"Project "<<i+1<<" mark: "<<project[i]<<"\n";}
+    fout<<"The best project mark is: "<<best_of_project<<"\n\n";
+    for(int i=0; i<no_of_exams; i++)
+        {fout<<"Exam "<<i+1<<" mark: "<<exam[i]<<"\n";}
+    fout<<"The weighted mean of the exam marks is: "<<weighted_mean<<"\n\n";
+    for(int i=0; i<no_of_labs; i++)
+        {fout<<"Lab "<<i+1<<" mark: "<<lab[i]<<"\n";}
+    fout<<"The average of the lab marks is: "<<average_of_labs<<"\n\n";
+    fout<<"Overall mark obtained is: "<<total_marks<<"\n\n";
+    fout<<comments;
+    fout.close();}
+
+double computeV(string s, double a[])
+{   if(s=="project")
+        {double temp=0;
+        for(int i=0; i<3; i++)
+            {if(a[i]>temp){temp=a[i];}}
+        return temp;}
+    else if(s=="lab")
+        {double sum=0; double average;
+        for(int i=0; i<5; i++){sum=sum+a[i];}
+         average=sum/5.00;
+         return average;}
+    else
+        {double top=0; double bottom=101; double weighted;
+        for(int i=0; i<2; i++)
+            {if (a[i]>top){top=a[i];}}
+        for(int i=0; i<2; i++)
+            {if (a[i]<bottom){bottom=a[i];}}
+        weighted=(top*2.00+bottom)/3.00;
+        return weighted;}}
+
+void fillA(double a[], int almt, string txt, double& compV, bool D)
+{   if (D==true){cout<<"That menu choice was already selected.\n";}
+    else
+    {for(int i=0; i<almt; i++)
+        {while(true)
+        {cout<<"Enter "<<txt<<" "<<i+1<<" mark> ";
+        cin>>a[i];
+        if(a[i]>=0 && a[i]<=100){break;}
+        else{cout<<"The mark entered is invalid. Please enter a value between 0 and 100 inclusive.\n";}}}
+        compV=computeV(txt, a);
+        cout<<"The "<<txt<<" mark considered for grading is: "<<compV<<"\n";}}
+
+void getname(string& name)
+{   cout<<"Enter the name of the student: ";
+    cin>> name;}
+
+void badinput(char a, char b){
+    if (a=='m'){cout<<b<<" is not a valid menu option\n";}
+    else if (a=='g'){cout<<"The mark entered is invalid. Values must be between 0 and 100 inclusive.\n";}
+    else if(a=='d'){cout<<"Please select all other choices before selecting option d\n";}}
+
+char done(double total, string& comments)
+{   if (total>=90){comments="Outstanding"; return 'a';}
+    else if (total>=80 && total<90){comments="Good"; return 'b';}
+    else if (total>=70 && total<80){comments="Needs to improve"; return 'c';}
+    else if (total>=60 && total<70){comments="Warning of probation"; return 'd';}
+    else{comments="Failed"; return 'f';}}
+
+void menu(){
+    char c='\0';
+    char grade;
+    bool l=false;
+    bool e=false;
+    bool p=false;
+    double lab[10];
+    double exam[10];
+    double project[10];
+    double average_of_labs;
+    double weighted_mean;
+    double best_of_projects;
+    double total_marks=0;
+    int no_of_labs=5;
+    int no_of_exams=2;
+    int no_of_projects=3;
+    string pro="project";
+    string lb="lab";
+    string ex="exam";
+    string name;
+    string comments;
+    string menuT="\nSelect a menu choice from the following options.\na. Project Marks\nb. Lab Marks\nc. Exam Marks\nd. Calculate total marks\n";
+    getname(name);
+    while((l==false || e==false || p==false) || (c!='d'))
+    {char m='m';
+    cout<<menuT;
+    cin>>c;
+    switch(c)
+        {case 'a':
+            fillA(project,no_of_projects, pro, best_of_projects, p);
+            if(p==false){p=!p;}
+            break;
+         case 'b':
+             fillA(lab, no_of_labs, lb, average_of_labs, l);
+             if(l==false){l=!l;}
+             break;
+         case 'c':
+             fillA(exam, no_of_exams, ex, weighted_mean, e);
+             if(e==false){e=!e;}
+             break;
+         case 'd':
+             if(l==false || e==false || p==false){m='d'; badinput(m, c);}
+             else
+                {total_marks=((average_of_labs*.25)+(weighted_mean*.5)+(best_of_projects*.25));
+                grade=done(total_marks, comments);
+                print(exam, no_of_exams, lab, no_of_labs, project, no_of_projects, weighted_mean, best_of_projects, average_of_labs, total_marks, grade, comments, name);}
+             break;
+         default:
+            badinput(m, c);
+            break;}}}
+
+int main(){
+    menu();
+return 0;
+}
--- EECS 138 Project III.cpp
+++ EECS 138 Project III.cpp
@@ -11,54 +11,63 @@
 #include <cstdlib>
 #include <string>
 #include <fstream>
-
+#include <cstdlib>
 
 using namespace std;
-
-
-void printarr(int[][4]);
-
-
-void printarr(char[][40]);
-
-const int no_of_books=100;
-int bookcount=0;
-
 
 struct book{
     string name;
     string genre;
     string year;
     string author;
-    string available;
-    };
+    bool available;
+};
 
-book newbook();
-book database[no_of_books];
-
+book newbook(int);
+void printbook(book [], int);
+void read(book [], int&);
+void comb(book [], int);
+void modify(book [], int);
+void destroy(book [], int&);
+void write(book [], int);
 
 int main(){
+    int bookcount=0;
+    book database[100];
     char c='\0';
-    string Menu="\nPlease select a choice from the options below.\na. Add a book to the database\nb. Search the database for a book\nc. Modify an existing book entry\nd. Delete an entry from the databse\ne. Save changes and exit the databse\n";
-
-
-    while(c!='e')
+    string Menu="\nPlease select a choice from the options below.\na. Add a book to the database\nb. Search the database for a book\nc. Modify an existing book entry\nd. Delete an entry from the databse\ne. Print contents of the database\nf. Save changes and exit the database\n";
+    read(database, bookcount);
+    while(c!='f')
     {cout<<"\nWelcome to the EECS 138 Library Database!\n";
     cout<<Menu;
     cin>>c;
     switch(c)
         {case 'a':
             cin.ignore();
-            database[bookcount]=newbook();
+            database[bookcount]=newbook(bookcount);
+            bookcount++;
             break;
         case 'b':
+            cin.ignore();
+            comb(database, bookcount);
             break;
         case 'c':
+            cin.ignore();
+            modify(database, bookcount);
             break;
         case 'd':
+            cin.ignore();
+            destroy(database, bookcount);
             break;
         case 'e':
+            printbook(database, bookcount);
+            break;
+        case 'f':
             cout<<"Saving changes...\n";
+            write(database, bookcount);
+            break;
+        default:
+            cout<<"Invalid input. Please try again.\n";
             break;
         }
 
@@ -66,47 +75,317 @@ int main(){
     return 0;
 }
 
-book newbook(){
-    book rec;
-    cout<<"Please enter the title of your book\n";
+void read(book database [], int& bookcount)
+{   ifstream fin("ProjectIII_Pickert.txt");
+    if(!fin.is_open()){cout<<"Database does not yet contain entries.\n"; return;}
+    else{
+        fin >> bookcount;
+        if(bookcount>100){
+        bookcount=100;
+        for(int i=0; i<bookcount; i++)
+           {getline(fin, database[i].name, '\t');
+            getline(fin, database[i].genre,'\t');
+            getline(fin, database[i].year,'\t');
+            getline(fin, database[i].author,'\t');
+            fin>>database[i].available;}}
+        else{
+            for(int i=0; i<bookcount; i++)
+           {getline(fin, database[i].name, '\t');
+            getline(fin, database[i].genre,'\t');
+            getline(fin, database[i].year,'\t');
+            getline(fin, database[i].author,'\t');
+            fin>>database[i].available;}}
+            fin.close();
+}}
+
+book newbook(int bookcount){
+    if(bookcount>=100){cout<<"\nBook database is full. Please delete entries before adding new ones.\n";}
+    else{book rec;
+    char a='\0';
+    cout<<"\nPlease enter the title of your book: ";
     getline(cin, rec.name);
-    cout<<"\nPlease enter the genre of your book\n";
+    cout<<"\nPlease enter the genre of your book: ";
     getline(cin, rec.genre);
-    cout<<"\nPlease enter the year of publication for your book\n";
+    cout<<"\nPlease enter the year of publication for your book: ";
     getline(cin, rec.year);
-    cout<<"\nPlease enter the author of your book\n";
+    cout<<"\nPlease enter the author of your book: ";
     getline(cin, rec.author);
-    cout<<"\nIs your book available for checkout? Y/N\n";
-    getline(cin, rec.available);
-    return rec;}
+    while(a!='y' && a!='n'){
+    cout<<"\nIs your book available for checkout? Y/N: ";
+    cin>>a;
+    if(a=='y'){rec.available=true;}
+    else if(a=='n'){rec.available=false;}
+    else{cout<<"Invalid input. Please enter 'y' or 'n'.";}}
+    return rec;}}
+
+void comb(book database [], int bookcount){
+    if(bookcount==0){cout<<"\nNo records exist in the database. Please input data first.\n";return;}
+    else{string M="\n\nPlease select a criterion for your book search:\na. Title\nb. Author Name\nc. Genre\n";
+    char c='\0';
+    bool hold=true;
+    while(hold){
+        string s;
+        char d='\0';
+        book filler [100];
+        int counter=0;
+        cout<<M;
+        cin>>c;
+        switch(c){
+            case 'a':
+                cout<<"\nPlease enter the title of the book: ";
+                cin.ignore();
+                getline(cin, s);
+                for(int i=0; i<bookcount; i++)
+                {if(s==database[i].name){
+                filler[counter].name=database[i].name;
+                filler[counter].genre=database[i].genre;
+                filler[counter].year=database[i].year;
+                filler[counter].author=database[i].author;
+                filler[counter].available=database[i].available;
+                counter++;}}
+                if(counter>0){cout<<"The following book/books matched your search: \n";printbook(filler, counter);}
+                else{cout<<"Book was not found.";}
+                cout<<"\nWould you like to conduct another search(Y/N)? ";
+                cin>>d;
+                if(d=='y'||d=='Y'){break;}
+                else if(d=='n'||d=='N'){hold=false;}
+                else{cout<<"\nInvalid input. Returning to search menu.\n";}
+                break;
+            case 'b':
+                cout<<"\nPlease enter the author's name: ";
+                cin.ignore();
+                getline(cin, s);
+                for(int i=0; i<bookcount; i++)
+                {if(s==database[i].author){
+                filler[counter].name=database[i].name;
+                filler[counter].genre=database[i].genre;
+                filler[counter].year=database[i].year;
+                filler[counter].author=database[i].author;
+                filler[counter].available=database[i].available;
+                counter++;}}
+                if(counter>0){cout<<"The following book/books matched your search: \n";printbook(filler, counter);}
+                else{cout<<"Book was not found.";}
+                cout<<"\nWould you like to conduct another search(Y/N)? ";
+                cin>>d;
+                if(d=='y'||d=='Y'){break;}
+                else if(d=='n'||d=='N'){hold=false;}
+                else{cout<<"\nInvalid input. Returning to search menu.\n";}
+                break;
+            case 'c':
+                cout<<"\nPlease enter the book's genre: ";
+                cin.ignore();
+                getline(cin, s);
+                for(int i=0; i<bookcount; i++)
+                {if(s==database[i].genre){
+                filler[counter].name=database[i].name;
+                filler[counter].genre=database[i].genre;
+                filler[counter].year=database[i].year;
+                filler[counter].author=database[i].author;
+                filler[counter].available=database[i].available;
+                counter++;}}
+                if(counter>0){cout<<"The following book/books matched your search: \n";printbook(filler, counter);}
+                else{cout<<"Book was not found.";}
+                cout<<"\nWould you like to conduct another search(Y/N)? ";
+                cin>>d;
+                if(d=='y'||d=='Y'){break;}
+                else if(d=='n'||d=='N'){hold=false;}
+                else{cout<<"\nInvalid input. Returning to search menu.\n";}
+                break;
+            default:
+                cout<<"Invalid input. Please try again.\n";
+                break;
+}}return;}}
+
+void modify(book database [], int bookcount){
+    if(bookcount==0){cout<<"\nNo records exist in the database to modify. Please input data first.\n";return;}
+    else{string s=""; book m [100]; book z [1]; bool hold; int smallcounter=0; string a="";
+        char c; string t; int mark1=0; int mark2=0;
+    cout<<"\nPlease enter the name of the book you would like to modify: ";
+    getline(cin, s);
+    for(int i=0; i<bookcount; i++)
+    {if(s==database[i].name){
+        m[smallcounter].name=database[i].name;
+        m[smallcounter].genre=database[i].genre;
+        m[smallcounter].year=database[i].year;
+        m[smallcounter].author=database[i].author;
+        m[smallcounter].available=database[i].available;
+        smallcounter++;mark1=i;}}
+    if(smallcounter==0){cout<<"\nBook was not found. Returning to main menu.\n";return;}
+    else if(smallcounter>1){cout<<"\nMultiple entries found.\n";
+        printbook(m ,smallcounter);
+        smallcounter=0;
+        cout<<"\nPlease narrow search by entering author's name: ";
+        getline(cin, a);
+        for(int i=0; i<bookcount; i++)
+            {if(a==database[i].author && s==database[i].name){
+                z[0].name=m[i].name;
+                z[0].genre=m[i].genre;
+                z[0].year=m[i].year;
+                z[0].author=m[i].author;
+                z[0].available=m[i].available;
+                smallcounter++;mark2=i;}}
+        cout<<smallcounter;
+        if(smallcounter==0){cout<<"\nNo author match found. Returning to main menu."; return;}
+        else{printbook(z, smallcounter);
+        while(hold){
+            char c='\0';
+            cout<<"\nSelect the book attribute that you'd like to modify:\na. Name\nb. Genre\nc. Year\nd. Author\ne. Availability\nf. Exit modification menu\n";
+            cin>>c;
+            switch(c){
+            case 'a':
+                cout<<"\nEnter the new name of the book: ";
+                cin.ignore();
+                getline(cin, t);
+                z[0].name=t;
+                database[mark2]=z[0];
+                cout<<"New record is: ";
+                printbook(z, smallcounter);
+                cout<<"\n Returning to modification menu...\n";
+                break;
+            case 'b':
+                cout<<"\nEnter the new genre of the book: ";
+                cin.ignore();
+                getline(cin, t);
+                z[0].genre=t;
+                database[mark2]=z[0];
+                cout<<"New record is: ";
+                printbook(z, smallcounter);
+                cout<<"\n Returning to modification menu...\n";
+                break;
+            case 'c':
+                cout<<"\nEnter the new publication year of the book: ";
+                cin.ignore();
+                getline(cin, t);
+                z[0].year=t;
+                database[mark2]=z[0];
+                cout<<"New record is: ";
+                printbook(z, smallcounter);
+                cout<<"\n Returning to modification menu...\n";
+                break;
+            case 'd':
+                cout<<"\nEnter the new author of the book: ";
+                cin.ignore();
+                getline(cin, t);
+                z[0].author=t;
+                database[mark2]=z[0];
+                cout<<"New record is: ";
+                printbook(z, smallcounter);
+                cout<<"\n Returning to modification menu...\n";
+                break;
+            case 'e':
+                cout<<"\nChanging availability...\n ";
+                z[0].available= !z[0].available;
+                database[mark2]=z[0];
+                cout<<"New record is: ";
+                printbook(z, smallcounter);
+                cout<<"\n Returning to modification menu...\n";
+                break;
+            case 'f':
+                cout<<"\nExiting menu...\n";
+                hold=false;
+                break;
+            default:
+                cout<<"\nInvalid input. Please try again.\n";}}}}
+    else{printbook(m, smallcounter);
+        while(hold){
+            char c='\0';
+            cout<<"\nSelect the book attribute that you'd like to modify:\na. Name\nb. Genre\nc. Year\nd. Author\ne. Availability\nf. Exit modification menu\n";
+            cin>>c;
+            switch(c){
+            case 'a':
+                cout<<"\nEnter the new name of the book: ";
+                cin.ignore();
+                getline(cin, t);
+                m[0].name=t;
+                database[mark1]=m[0];
+                cout<<"New record is: ";
+                printbook(m, smallcounter);
+                cout<<"\n Returning to modification menu...\n";
+                break;
+            case 'b':
+                cout<<"\nEnter the new genre of the book: ";
+                cin.ignore();
+                getline(cin, t);
+                m[0].genre=t;
+                database[mark1]=m[0];
+                cout<<"New record is: ";
+                printbook(m, smallcounter);
+                cout<<"\n Returning to modification menu...\n";
+                break;
+            case 'c':
+                cout<<"\nEnter the new publication year of the book: ";
+                cin.ignore();
+                getline(cin, t);
+                m[0].year=t;
+                database[mark1]=m[0];
+                cout<<"New record is: ";
+                printbook(m, smallcounter);
+                cout<<"\n Returning to modification menu...\n";
+                break;
+            case 'd':
+                cout<<"\nEnter the new author of the book: ";
+                cin.ignore();
+                getline(cin, t);
+                m[0].author=t;
+                database[mark1]=m[0];
+                cout<<"New record is: ";
+                cout<<mark1;
+                cout<<"\n"<<bookcount;
+                printbook(m, smallcounter);
+                cout<<"\n Returning to modification menu...\n";
+                break;
+            case 'e':
+                cout<<"\nChanging availability...\n ";
+                m[0].available= !m[0].available;
+                database[mark1]=m[0];
+                cout<<"New record is: ";
+                printbook(m, smallcounter);
+                cout<<"\n Returning to modification menu...\n";
+                break;
+            case 'f':
+                cout<<"\nExiting menu...\n";
+                hold=false;
+                break;
+            default:
+                cout<<"\nInvalid input. Please try again.\n";}}}}return;}
 
 
-void enter(char a[][40], int number, int names)
-{   cin.ignore();
-    for(int i=0; i<names; i++)
-        {cout<<"Enter student "<<i+1<<"'s name: ";
-        cin.getline(a[i], number);}}
-
-
-
-
-
-void printarr(int a[][4]){
-    for(int i=0;i<5;i++){
-        for(int j=0; j<4; j++)
-        {cout << a[i][j] <<" ";}
-    cout <<"\n\n";}
-    return;
+void printbook(book database [], int counter){
+    for(int i=0; i<counter; i++)
+       {cout<<"\nBook title: "<<database[i].name<<"\n";
+        cout<<"Book genre: "<<database[i].genre<<"\n";
+        cout<<"Year published: "<<database[i].year<<"\n";
+        cout<<"Book author: "<<database[i].author<<"\n";
+        cout<<"Available?: ";
+        if(database[i].available==true){cout<<"Y\n";}
+        else{cout<<"N\n";}}
 }
 
-void printarr(char a[][40]){
-    for(int i=0;i<5; i++){
-        for(int j=0; j<40; j++)
-        {{if(a[i][j]==000)
-        {break;}
-        else
-        cout << a[i][j];}}
-    cout <<"\n\n";}
-    return;
-}
+void destroy(book database [], int& bookcount){
+    if(bookcount==0){cout<<"\nNo records exist in the database to delete. Please input data first.\n";return;}
+    else{string d;
+    cout<<"Please enter the name of the book you would like to delete from the database: ";
+    getline(cin, d);
+    for(int i=0; i<bookcount; i++)
+    {if(d==database[i].name){
+        database[i].name=database[bookcount-1].name;
+        database[i].genre=database[bookcount-1].genre;
+        database[i].year=database[bookcount-1].year;
+        database[i].author=database[bookcount-1].author;
+        database[i].available=database[bookcount-1].available;
+        bookcount--;
+        cout<<"\nBook was removed from database.\n";return;}}
+    cout<<"Book was not found.";
+}}
 
+
+void write(book database[], int number)
+{   ofstream fout("ProjectIII_Pickert.txt");
+    fout<<number;
+    for(int i=0; i<number; i++)
+       {fout<<database[i].name<<"\t";
+        fout<<database[i].genre<<"\t";
+        fout<<database[i].year<<"\t";
+        fout<<database[i].author<<"\t";
+        fout<<database[i].available;}
+        fout.close();}
