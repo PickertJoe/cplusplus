e0d1a902a2c49b42f626129b66561bf9b68c804e
Joseph Pickert <pickertjoe@gmail.com>
2019-07-17 11:37:43 -0600

    Mostly implimented Sudoku Solver

--- projects/puzzle_solver_app.cpp
+++ projects/puzzle_solver_app.cpp
@@ -5,7 +5,6 @@
 #include <cmath>
 #include <iomanip>
 
-
 void fibonacci_input();
 void fibonacci(int n);
 void prime_factorization();
@@ -21,65 +20,9 @@ double distance_calc(double, double, double, double);
 void sudoku();
 void infix();
 
-int main(){
-    //Main menu that allows user to choose puzzle/problem they'd like to solve
-    int c;
-    const char *welcome="~~~Welcome to Joe's C++ Puzzle Solving Program!~~~";
-    while(c!=11){
-        std::cout<<std::endl<<std::endl<<welcome<<std::endl<<std::endl;
-        std::cout<<"Please select an option from the menu below: \n";
-        std::cout<<"1) Fibonacci Sequence Generator \n";
-        std::cout<<"2) Prime Number Factorization \n";
-        std::cout<<"3) Next Prime Number Generator \n";
-        std::cout<<"4) Palindrome Analyzer \n";
-        std::cout<<"5) Payment Change Calculator \n";
-        std::cout<<"6) Binary/Decimal Conversion Calculator \n";
-        std::cout<<"7) Reverse a String \n";
-        std::cout<<"8) Geographic Distance Calculator \n";
-        std::cout<<"9) Sudoku Puzzle Solver \n";
-        std::cout<<"10) Prefix/Infix/Postfix Converter + Calculator \n";
-        std::cout<<"11) Exit program \n";
-        std::cin>>c;
-        switch(c){
-        case 1:
-            fibonacci_input();
-            break;
-        case 2:
-            prime_factorization();
-            break;
-        case 3:
-            next_prime();
-            break;
-        case 4:
-            palindrome();
-            break;
-        case 5:
-            change_calculator();
-            break;
-        case 6:
-            binary_calculator();
-            break;
-        case 7:
-            reverse_string();
-            break;
-        case 8:
-            distance_main();
-            break;
-        case 9:
-            sudoku();
-            break;
-        case 10:
-            infix();
-            break;
-        case 11:
-            std::cout<<"Closing program..."<<std::endl;
-            return 0;
-        default:
-            std::cout<<"Invalid input. Please try again."<<std::endl;
-
-        }
-    }
-}
+//###################################
+//BEGIN SECTION OF FIBONACCI SEQUENCE CODE
+//###################################
 
 void fibonacci_input(){
     //Collects user input for number of desired iterations
@@ -128,6 +71,9 @@ void fibonacci(int iterations){
     std::cout<<"]";
 }
 
+//###################################
+//BEGIN SECTION OF DISTANCE CALCULATOR CODE
+//###################################
 
 void distance_main(){
     //Initializes appropriate variables and orchestrates distance calculation functions
@@ -219,6 +165,135 @@ double distance_calc(double lat1, double long1, double lat2, double long2){
     return distance;
 }
 
+//###################################
+//BEGIN SECTION OF SUDOKU SOLVER CODE
+//###################################
+
+//Declaration for function that will search the puzzle for empty cells
+bool FindEmptyCells(int grid[9][9], int &row, int &col);
+
+//Declaration for funcion that will verify that attempted number satisfies puzzle rules
+bool NumberWorks(int grid[9][9], int row, int col, int guess);
+
+//Function that takes user-input for puzzle and uses backtracking and recursion to solve
+bool SudokuSolver(int grid[9][9]){
+        int row, col;
+
+        //Puzzle if solved when all boxes are filled
+        if(FindEmptyCells(grid,row,col) == false){
+            return true;
+        }
+
+        //Looping through the possible solutions to each cell in the puzzle
+        for(int i=1; i<=9; i++){
+            //Testing number to see if it conflicts with row/col/cell
+            if(NumberWorks(grid, row, col, i) == true){
+                //If compatible, assign to cell
+                grid[row][col] = i;
+
+                //Call function recursively; if it passes as true, puzzle solved!
+                if(SudokuSolver(grid) == true){
+                    return true;
+                }
+
+                //If a conflict emerges, reset cell to zero and try next
+                grid[row][col] = 0;
+            }
+        }
+        //If all options are exhausted for cell, trigger backtracking
+        return false;
+}
+
+/*This function searches the puzzle for empty cells. If an empty cell is encountered, the row
+and column that have been called by reference are set to this cell and the function returns true
+If no empty cells are found, the function is returned false and the puzzle is solved!*/
+
+bool FindEmptyCells(int grid[9][9], int &row, int &col){
+    for(row = 0; row<9; row++){
+        for(col = 0; col<9; col++){
+            if(grid[row][col] == 0){
+                return true;
+            }
+        }
+    }
+    //If no empty cells are encountered:
+    return false;
+}
+
+//Function determines if guess exists in current row
+bool InRow(int grid[9][9], int row, int guess){
+    for(int col=0; col<9; col++){
+        if(grid[row][col] == guess){
+            return true;
+        }
+    }
+    //If number is not in current row:
+    return false;
+}
+
+//Function determines if guess exists in current column
+bool InCol(int grid[9][9], int col, int guess){
+    for(int row=0; row<9; row++){
+        if(grid[row][col] == guess){
+            return true;
+        }
+    }
+    //If number is not in current column:
+    return false;
+}
+
+//Function determines if guess exists in current 3x3 box
+bool InBox(int grid[9][9], int StartRow, int StartCol, int guess){
+    for(int row=0; row<3; row++){
+        for(int col=0; col<3; col++){
+            if(grid[row+StartRow][col+StartCol] == guess){
+                return true;
+            }
+        }
+    }
+    //If number is not in current 3x3 box:
+    return false;
+}
+
+//Function calls all three previous tests to determine if guess fulfills puzzle rules
+bool NumberWorks(int grid[9][9], int row, int col, int guess){
+    return !InRow(grid, row, guess) &&
+            !InCol(grid, row, guess) &&
+            !InBox(grid, row - row % 3, col- col % 3, guess) &&
+            grid[row][col] == 0;
+}
+
+//Function that prints the Sudoku grid
+void SudokuPrint(int grid[9][9]){
+    for(int row=0; row<9; row++){
+        for(int col=0; col<9; col++){
+            std::cout<<grid[row][col]<<" ";
+        }
+        std::cout<<std::endl;
+    }
+}
+
+void sudoku_main(){
+    //Driver function for sudoku solver app
+    int grid[9][9] = {{3, 0, 6, 5, 0, 8, 4, 0, 0},
+                      {5, 2, 0, 0, 0, 0, 0, 0, 0},
+                      {0, 8, 7, 0, 0, 0, 0, 3, 1},
+                      {0, 0, 3, 0, 1, 0, 0, 8, 0},
+                      {9, 0, 0, 8, 6, 3, 0, 0, 5},
+                      {0, 5, 0, 0, 9, 0, 6, 0, 0},
+                      {1, 3, 0, 0, 0, 0, 2, 5, 0},
+                      {0, 0, 0, 0, 0, 0, 0, 7, 4},
+                      {0, 0, 5, 2, 0, 6, 3, 0, 0}};
+
+    if (SudokuSolver(grid) == true){
+        SudokuPrint(grid);
+    }
+    else{
+        std::cout<<"Sorry, but your puzzle couldn't be solved. Please check input."<<std::endl;
+    }
+
+}
+
 void prime_factorization(){};
 void next_prime(){};
 void palindrome(){};
@@ -227,3 +302,64 @@ void binary_calculator(){};
 void reverse_string(){};
 void sudoku(){};
 void infix(){};
+
+int main(){
+    //Main menu that allows user to choose puzzle/problem they'd like to solve
+    int c=0;
+    const char *welcome="~~~Welcome to Joe's C++ Puzzle Solving Program!~~~";
+    while(c!=11){
+        std::cout<<std::endl<<std::endl<<welcome<<std::endl<<std::endl;
+        std::cout<<"Please select an option from the menu below: \n";
+        std::cout<<"1) Fibonacci Sequence Generator \n";
+        std::cout<<"2) Prime Number Factorization \n";
+        std::cout<<"3) Next Prime Number Generator \n";
+        std::cout<<"4) Palindrome Analyzer \n";
+        std::cout<<"5) Payment Change Calculator \n";
+        std::cout<<"6) Binary/Decimal Conversion Calculator \n";
+        std::cout<<"7) Reverse a String \n";
+        std::cout<<"8) Geographic Distance Calculator \n";
+        std::cout<<"9) Sudoku Puzzle Solver \n";
+        std::cout<<"10) Prefix/Infix/Postfix Converter + Calculator \n";
+        std::cout<<"11) Exit program \n";
+        std::cin>>c;
+        switch(c){
+        case 1:
+            fibonacci_input();
+            break;
+        case 2:
+            prime_factorization();
+            break;
+        case 3:
+            next_prime();
+            break;
+        case 4:
+            palindrome();
+            break;
+        case 5:
+            change_calculator();
+            break;
+        case 6:
+            binary_calculator();
+            break;
+        case 7:
+            reverse_string();
+            break;
+        case 8:
+            distance_main();
+            break;
+        case 9:
+            sudoku_main();
+            break;
+        case 10:
+            infix();
+            break;
+        case 11:
+            std::cout<<"Closing program..."<<std::endl;
+            return 0;
+        default:
+            std::cout<<"Invalid input. Please try again."<<std::endl;
+
+        }
+    }
+    return 0;
+}
